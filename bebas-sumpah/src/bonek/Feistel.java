060c40c4b286cf57a067ed41e1dce3c3b772966a
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-04-24 03:31:22 +0700

    add implementation: BEA : feistel

--- bebas-sumpah/src/bonek/Feistel.java
+++ bebas-sumpah/src/bonek/Feistel.java
@@ -10,5 +10,39 @@ package bonek;
  * @author Luqman A. Siswanto
  */
 public class Feistel {
+  Keygen keygen = new Keygen();
   
+  public Feistel() {
+    
+  }
+  
+  public Block encrypt(Block plain, int[] key) {
+    Block fi = new Block(4);
+    Block se = new Block(4);
+    Block newfi = new Block(4);
+    Block newse = new Block(4);
+    Pair<Block, Block> tmp = plain.split();
+    fi = tmp.first;
+    se = tmp.second;
+    newfi = se;
+    newse = se.f_encrypt(new Block(4, key)).xor(fi);
+    Block ret = new Block(8);
+    ret.combine(newfi, newse);
+    return ret;
+  }
+  
+  public Block decrypt(Block cipher, int[] key) {
+    Block fi = new Block(4);
+    Block se = new Block(4);
+    Block newfi = new Block(4);
+    Block newse = new Block(4);
+    Pair<Block, Block> tmp = cipher.split();
+    fi = tmp.first;
+    se = tmp.second;
+    newse = fi;
+    newfi = fi.f_encrypt(new Block(4, key)).xor(se);
+    Block ret = new Block(8);
+    ret.combine(newfi, newse);
+    return ret;
+  }
 }

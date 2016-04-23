a8f1454b7cec0c4895647f81fbb2743f2dc923e9
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-04-24 04:39:14 +0700

    add SHA1 implementation

--- bebas-sumpah/src/Main.java
+++ bebas-sumpah/src/Main.java
@@ -1,3 +1,6 @@
+
+import java.math.BigInteger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -25,6 +28,8 @@ public class Main {
     
     Kelas k = new Kelas();
     k.a().b();
+    BigInteger big = new BigInteger("10", 16);
+    System.out.println(big);
   }
   
   public static class Kelas {
---
+++ cleaner.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string buf;
+  while(getline(cin, buf)) {
+    buf = buf.substr(6);
+    cout << buf << endl;
+  }
+  return 0;
+}
